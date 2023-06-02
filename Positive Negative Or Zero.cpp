b5b9ad69874e2a5bea26fee418a11eb6587684a8
Shine jose <89456422+shhinne@users.noreply.github.com>
2023-06-02 19:58:06 +0530

    Add files via upload

---
+++ Positive Negative Or Zero.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+//Compiler version g++ 6.3.0
+
+int main()
+{
+  int n;
+  cout<<"\n enter a number: " ;
+  cin>> n;
+  if (n>0)
+  cout<<"\n the number is positive";
+  else if (n<0)
+  cout<<"\n the number is negative";
+  else if (n==0)
+  cout<<"\n the number is zero";
+  return 0;
+  }
+  
+  
+  
+ 
